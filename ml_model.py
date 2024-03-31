6fe864e92767eb8c19af8369fbf6b4774ad50a0f
rknhub <rkn.bigshot@gmail.com>
2024-03-31 18:49:10 +0400

    added 3 new ml models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("addding all code related to ML model here")
+
+def mode1():
+    print ("adding code for ML model1")
+
+def mode2():
+    print ("adding code for ML model2")
+
+def mode3():
+    print ("adding code for ML model3")
+    
